85aa7ca40e78a03c548dad4fbaa9dbb3a9036bfb
Billy272 <billylaroi@gmail.com>
2025-02-11 23:01:09 +0300

    Add ESLint configuration and implement form state management for invoice creation and editing

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,25 +4,44 @@ import { date, z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { error } from 'console';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please Select Customer',
+    }),
+    amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
 })
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 const CreateInvoice = FormSchema.omit({ id:true, date:true});
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status} = CreateInvoice.parse({
+export type State = {
+    error?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+
+export async function createInvoice(prevState: State ,formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
-        amount: Number(formData.get('amount')),
+        amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return { ...prevState, error: validatedFields.error.flatten().fieldErrors };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -35,13 +54,21 @@ export async function createInvoice(formData: FormData) {
 };
 
 const UpdateInvoice = FormSchema.omit({date:true});
-export async function updateInvoice( id: string,formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice( id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            error: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing required fields',
+        }
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
  
     try {
